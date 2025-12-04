40a3f6e04fe19eb21276dea3fd9fea9a93b62cfb
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-12-04 12:10:33 -0500

    Update card_front_binop.gd

--- decrementor/Scenes/card_front_binop.gd
+++ decrementor/Scenes/card_front_binop.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var random_number = randi() % 4
+var value
 
 @onready var addition = $Area2D/CollisionShape2D/BaseOpCard/Plus
 @onready var subtraction = $Area2D/CollisionShape2D/BaseOpCard/Minus
@@ -9,16 +10,16 @@ var random_number = randi() % 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(random_number)
 	if (random_number == 0):
 		addition.visible = true
+		value = '+'
 	if (random_number == 1):
 		subtraction.visible = true
+		value = '-'
 	if (random_number == 2):
 		multiply.visible = true
+		value = 'x'
 	if (random_number == 3):
 		division.visible = true
-	#label.text = "/"
-	#label.text = str(random_number)
-	#label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	#
+		value = '/'
+	print(value) 
