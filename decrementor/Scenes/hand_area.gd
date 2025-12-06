b4f9430a7ad7b2bf4a748e361be82aaab3d7e6e1
Phykio <naingp8705@gmail.com>
2025-12-05 21:24:28 -0500

    hi

--- decrementor/Scenes/hand_area.gd
+++ decrementor/Scenes/hand_area.gd
@@ -5,7 +5,6 @@ const OPERATOR_GUARANTEE_DRAWS = 6
 
 var card_container: Node
 var equation_area: Node
-var draws_since_operator_check: int = 0
 
 func _ready() -> void:
 	randomize()
@@ -18,24 +17,6 @@ func create_deck() -> void:
 		equation_area = parent.get_node_or_null("Equation Area")
 	
 	Global.create_deck()
-
-func is_operator_card(card: Control) -> bool:
-	var base_op_card = card.get_node_or_null("Area2D/CollisionShape2D/BaseOpCard")
-	return base_op_card != null
-
-func has_operator_in_hand() -> bool:
-	for child in card_container.get_children():
-		if is_operator_card(child):
-			return true
-	return false
-
-func get_operator_indices_from_deck() -> Array:
-	var operator_indices = []
-	for i in range(Global.deck.size()):
-		var card = Global.deck[i]
-		if is_operator_card(card):
-			operator_indices.append(i)
-	return operator_indices
 
 func draw_card() -> Control:
 	var hand_card_count = card_container.get_child_count()
@@ -53,34 +34,12 @@ func draw_card() -> Control:
 		Global.reshuffle_discard_into_deck()
 		if Global.deck.size() == 0:
 			return null
-	
-	var needs_operator = false
-	if not has_operator_in_hand():
-		draws_since_operator_check += 1
-		if draws_since_operator_check >= OPERATOR_GUARANTEE_DRAWS:
-			needs_operator = true
-	else:
-		draws_since_operator_check = 0
-	
+
 	var card: Control
 	var idx: int
-	
-	if needs_operator:
-		var operator_indices = get_operator_indices_from_deck()
-		if operator_indices.size() > 0:
-			var random_op_idx = randi() % operator_indices.size()
-			idx = operator_indices[random_op_idx]
-			card = Global.deck[idx]
-			Global.deck.remove_at(idx)
-			draws_since_operator_check = 0
-		else:
-			idx = randi() % Global.deck.size()
-			card = Global.deck[idx]
-			Global.deck.remove_at(idx)
-	else:
-		idx = randi() % Global.deck.size()
-		card = Global.deck[idx]
-		Global.deck.remove_at(idx)
+	idx = randi() % Global.deck.size()
+	card = Global.deck[idx]
+	Global.deck.remove_at(idx)
 	
 	var card_name_before = get_card_name(card)
 	print("BEFORE adding to scene: ", card_name_before)
