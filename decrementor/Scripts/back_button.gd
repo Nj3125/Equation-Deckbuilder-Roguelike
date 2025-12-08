f8a1eaaa8756c07cfcb9ea9f07253268f4eb127f
Phykio <naingp8705@gmail.com>
2025-12-07 20:27:53 -0500

    FINAL

--- decrementor/Scripts/back_button.gd
+++ decrementor/Scripts/back_button.gd
@@ -5,4 +5,5 @@ func _on_pressed_leaderboard():
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 func _on_pressed_defeat():
 	GameState.player_health = 100
+	GameState.enemy_health = 50
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
